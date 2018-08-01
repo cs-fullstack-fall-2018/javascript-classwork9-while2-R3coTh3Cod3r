6cf4e3682d58684925ceb38037e29ea76340e69e
Mareco Nabors <naborsmarico99@gmail.com>
2018-07-31 19:21:49 -0500

    While2

---
+++ ex22.js
@@ -0,0 +1,25 @@
+function main() {
+
+    var personInput = prompt("Enter a word or press 'q' to quit ");
+    while(personInput !="q")
+
+    {
+var personInputer=prompt("Please try again :)");
+alert(personInput)
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+main();
---
+++ ex23.js
@@ -0,0 +1,18 @@
+function main() {
+var password = "XTheCode";
+var userInput= prompt("Enter the password");
+while (userInput!==password)  {
+
+
+}
+}
+var userInput=prompt("YOU SHALL NOT PASS!!!-_-");
+alert(userInput);
+
+
+
+
+
+
+
+main();
